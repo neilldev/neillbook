161bce492e7ee7d2d5fc971435b977b4239b4d96
neilldev <neill.blanchard@gmx.com>
2014-02-28 12:04:28 +0100

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Neillbook::Application.routes.draw do
+  devise_for :users 
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
 
-  root :to => "statuses#new"
-  devise_for :users  
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
+  get 'Update', to: 'statuses#new', as: :update
+  root :to => "statuses#index"
+  
+  # A logout link not as a get rqst = more secure 
+  # <%= log_out, destroy_user_session_path, method: :delete, data: "Are you sure?" %>
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
